c105f49bdc633a307b0a2df8bee377b076b8d056
Scott Perry <github@numist.net>
2012-06-20 23:28:12 -0800

    trim series names and add series links to posts I have no internet connection, so I'm not fucking around with HTML formatting yet, but it's a start.

--- bin/make-post.php
+++ bin/make-post.php
@@ -6,7 +6,7 @@ define("BLOGROOT", dirname($argv[0])."/../");
 define("WEBROOT", "../../".($preview ? "../" : ""));
 
 require_once(BLOGROOT."lib/intro.inc");
-require_once(BLOGROOT."lib/post.class.inc");
+require_once(BLOGROOT."lib/series.class.inc");
 
 assert('$argc == 2 || $argc == 3');
 assert('is_dir(BLOGROOT."data/".$argv[1])');
@@ -31,6 +31,35 @@ function postwidth() {
 require(BLOGROOT."templates/header.inc");
 
 echo poast($post, true);
+
+$series = $post->series();
+shuffle($series);
+?>
+<div class="entry">
+	<div class="entrybody">
+		<p>This post is part of the series
+			<? for($i = 0; $i < count($series); $i++) {
+				// Three or more items, Oxford commas!
+				if($i && count($series) > 2) {
+					echo ",";
+				}
+				if($i) {
+					echo " ";
+				}
+				// Last item, and it together
+				if($i && $i == count($series) - 1) {
+					echo "and ";
+				}
+				?><a href="#"><?= $series[$i]->title() ?></a><?
+			}
+		?>.</p>
+	</div>
+	<br style="clear:both;">
+	<br style="clear:both;">
+</div>
+
+
+<?
 
 require(BLOGROOT."templates/footer.inc");
 
